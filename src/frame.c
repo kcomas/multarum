ba45a0a7726a9ed1561771600f05bbb70ad80f0e
kcomas <kevin.comas.git@gmail.com>
2018-08-01 21:46:50 -0400

    add lang example

--- src/frame.c
+++ src/frame.c
@@ -3,11 +3,9 @@
 
 mt_frame* mt_frame_top_copy(const mt_frame* const frames, size_t f_len, size_t count) {
     mt_frame* new_frames = (mt_frame*) malloc(sizeof(mt_frame) * count);
-    while (count > 0) {
-        new_frames[count - 1] = frames[f_len - 1];
+    for (; count > 0; count--) {
+        new_frames[count - 1] = frames[--f_len];
         new_frames[count - 1].mod->ref_count++;
-        f_len--;
-        count--;
     }
     return new_frames;
 }
