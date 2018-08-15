f43d7ae13d1fae545c1d037b4fd47c1115f4abbb
kcomas <kevin.comas.git@gmail.com>
2018-08-15 18:46:35 -0400

    split dis into own fn

--- src/mod.c
+++ src/mod.c
@@ -79,11 +79,79 @@ static size_t mt_mod_single_byte_cmd(const mt_mod* mod, size_t i) {
     return i + 1;
 }
 
-void mt_mod_dis(const mt_mod* const mod) {
+static size_t mt_print_next_op(const mt_mod* const mod, size_t i, size_t count_total) {
     uint32_t mt_jmp;
     int64_t mt_int;
     double mt_float;
 
+    printf("%lu: ", i);
+    mt_mod_space_bytes(i, count_total);
+    switch (mod->bytes[i]) {
+        case mt_pfx(NOP):
+        case mt_pfx(ADD):
+        case mt_pfx(SUB):
+        case mt_pfx(EQ):
+        case mt_pfx(LD_SELF):
+        case mt_pfx(RET):
+        case mt_pfx(HALT):
+            i = mt_mod_single_byte_cmd(mod, i);
+            break;
+        case mt_pfx(PUSH):
+            mt_print_byte_hex(mod, i, 1);
+            switch (mod->bytes[++i]) {
+                case mt_pfx(NULL):
+                    mt_print_byte_hex(mod, i, 1);
+                    mt_mod_print_even_spaces(2);
+                    printf("PUSH NULL\n");
+                    i++;
+                    break;
+                case mt_pfx(BOOL):
+                    mt_print_byte_hex(mod, i, 2);
+                    mt_mod_print_even_spaces(3);
+                    printf("PUSH %s\n", mod->bytes[++i] == true ? "true" : "false");
+                    i++;
+                    break;
+                case mt_pfx(CHAR):
+                    mt_print_byte_hex(mod, i, 2);
+                    mt_mod_print_even_spaces(3);
+                    printf("PUSH %c\n", mod->bytes[++i]);
+                    i++;
+                    break;
+                case mt_pfx(INT):
+                    mt_print_byte_hex(mod, i, 9);
+                    mt_mod_get_bytes(mod, &mt_int, sizeof(int64_t), i);
+                    printf("PUSH %li\n", mt_int);
+                    break;
+                case mt_pfx(FLOAT):
+                    mt_print_byte_hex(mod, i, 9);
+                    mt_mod_get_bytes(mod, &mt_float, sizeof(double), i);
+                    printf("PUSH %lf\n", mt_float);
+                    break;
+            }
+            break;
+        case mt_pfx(JMP):
+            i = mt_mod_op_w_data(mod, i, sizeof(uint32_t), &mt_jmp);
+            printf("JMP %d\n", mt_jmp);
+            break;
+        case mt_pfx(JMPF):
+            i = mt_mod_op_w_data(mod, i, sizeof(uint32_t), &mt_jmp);
+            printf("JMPF %d\n", mt_jmp);
+            break;
+        case mt_pfx(LD_FN):
+        case mt_pfx(LD_ARG):
+        case mt_pfx(CALL_SELF):
+        case mt_pfx(CALL):
+            i = mt_mod_op2(mod, i);
+            break;
+        default:
+            i++;
+            printf("\n");
+            break;
+    }
+    return i;
+}
+
+void mt_mod_dis(const mt_mod* const mod) {
     size_t i = 0;
     size_t count_total = mt_mod_num_len(mod->len);
     while (i < mod->len) {
@@ -93,69 +161,6 @@ void mt_mod_dis(const mt_mod* const mod) {
                 break;
             }
         }
-        printf("%lu: ", i);
-        mt_mod_space_bytes(i, count_total);
-        switch (mod->bytes[i]) {
-            case mt_pfx(NOP):
-            case mt_pfx(ADD):
-            case mt_pfx(SUB):
-            case mt_pfx(EQ):
-            case mt_pfx(LD_SELF):
-            case mt_pfx(RET):
-            case mt_pfx(HALT):
-                i = mt_mod_single_byte_cmd(mod, i);
-                break;
-            case mt_pfx(PUSH):
-                mt_print_byte_hex(mod, i, 1);
-                switch (mod->bytes[++i]) {
-                    case mt_pfx(NULL):
-                        mt_print_byte_hex(mod, i, 1);
-                        mt_mod_print_even_spaces(2);
-                        printf("PUSH NULL\n");
-                        i++;
-                        break;
-                    case mt_pfx(BOOL):
-                        mt_print_byte_hex(mod, i, 2);
-                        mt_mod_print_even_spaces(3);
-                        printf("PUSH %s\n", mod->bytes[++i] == true ? "true" : "false");
-                        i++;
-                        break;
-                    case mt_pfx(CHAR):
-                        mt_print_byte_hex(mod, i, 2);
-                        mt_mod_print_even_spaces(3);
-                        printf("PUSH %c\n", mod->bytes[++i]);
-                        i++;
-                        break;
-                    case mt_pfx(INT):
-                        mt_print_byte_hex(mod, i, 9);
-                        mt_mod_get_bytes(mod, &mt_int, sizeof(int64_t), i);
-                        printf("PUSH %li\n", mt_int);
-                        break;
-                    case mt_pfx(FLOAT):
-                        mt_print_byte_hex(mod, i, 9);
-                        mt_mod_get_bytes(mod, &mt_float, sizeof(double), i);
-                        printf("PUSH %lf\n", mt_float);
-                        break;
-                }
-                break;
-            case mt_pfx(JMP):
-                i = mt_mod_op_w_data(mod, i, sizeof(uint32_t), &mt_jmp);
-                printf("JMP %d\n", mt_jmp);
-                break;
-            case mt_pfx(JMPF):
-                i = mt_mod_op_w_data(mod, i, sizeof(uint32_t), &mt_jmp);
-                printf("JMPF %d\n", mt_jmp);
-                break;
-            case mt_pfx(LD_FN):
-            case mt_pfx(LD_ARG):
-            case mt_pfx(CALL_SELF):
-            case mt_pfx(CALL):
-                i = mt_mod_op2(mod, i);
-                break;
-            default:
-                i++;
-                printf("\n");
-                break;
-        }
+        i = mt_print_next_op(mod, i, count_total);
     }
 }
