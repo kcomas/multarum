24f45ca73f843209eb9775f7e1639c291f7d9126
kcomas <kevin.comas.git@gmail.com>
2018-08-22 19:19:14 -0400

    init buf iter

--- src/token.c
+++ src/token.c
@@ -3,15 +3,17 @@
 
 void mt_token_state_init(mt_token_state* const state) {
     state->state = mt_token_state(NOTHING);
-    // @TODO init buf iter
     state->head = NULL;
     state->tail = NULL;
 }
 
 mt_var mt_tokenize_buf(mt_token_state* const state, const mt_buf* const buf) {
     mt_buf_iter char_iter;
+    mt_char cur_char;
+
+    mt_buf_iter_init(buf, &state->iter);
     /**
-    while (state->buf_pos < buf->len) {
+    while (state->iter->buf_pos < buf->len) {
 
     }
     */
