19b6505964bbc1f279ecaa5ae39161dc607e5bc3
kcomas <kevin.comas.git@gmail.com>
2018-08-23 21:04:04 -0400

    sep loop from next char

--- src/token.c
+++ src/token.c
@@ -9,9 +9,11 @@ void mt_token_state_init(mt_token_state* const state) {
 
 mt_var mt_tokenize_buf(mt_token_state* const state, const mt_buf* const buf) {
     mt_char cur_char;
+    bool has_chars = true;
 
     mt_buf_iter_init(buf, &state->iter);
-    while (mt_buf_iter_next(&state->iter, &cur_char)) {
+    while (has_chars) {
+        has_chars = mt_buf_iter_next(&state->iter, &cur_char);
         printf("%c\n", cur_char.a);
     }
     return mt_var_bool(true);
