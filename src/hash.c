3b492b4ae46e9c148439dcc2a5b0c1189084864c
kcomas <kevin.comas.git@gmail.com>
2018-09-23 19:23:10 -0400

    init hash

--- src/hash.c
+++ src/hash.c
@@ -1,6 +1,11 @@
 
 #include "hash.h"
 
-mt_hash_node* mt_hash_init(size_t buckets) {
-    return (mt_hash_node*) malloc(sizeof(mt_hash_node) * buckets);
+mt_hash* mt_hash_init(size_t num_buckets) {
+    mt_hash* h = (mt_hash*) malloc(sizeof(mt_hash));
+    h->ref_count = 0;
+    h->len = 0;
+    h->_bsize = num_buckets;
+    h->buckets = (mt_hash_node*) malloc(sizeof(mt_hash_node) * num_buckets);
+    return h;
 }
--- src/hash.h
+++ src/hash.h
@@ -4,15 +4,19 @@
 
 #include "var.h"
 
-#define MT_HASH_FULL_RESIZE 0.70
-
 typedef struct _mt_hash_node {
-   int32_t key;
-   uint8_t* name;
-   struct _mt_hash_node* next;
-   mt_var value;
+    int32_t key;
+    uint8_t* name;
+    struct _mt_hash_node* next;
+    mt_var value;
 } mt_hash_node;
 
-mt_hash_node* mt_hash_init(size_t buckets);
+typedef struct {
+    uint32_t ref_count;
+    size_t len, _bsize; // len can be larger then _bsize
+    mt_hash_node* buckets;
+} mt_hash;
+
+mt_hash* mt_hash_init(size_t num_buckets);
 
 #endif
