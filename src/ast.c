5056446d34d78fc00309ce1c4423e32792016a35
kcomas <kevin.comas.git@gmail.com>
2018-10-31 21:54:49 -0400

    load assign for print

--- src/ast.c
+++ src/ast.c
@@ -25,6 +25,13 @@ static inline mt_ast* mt_ast_create_node(mt_ast_type type, mt_ast_node node) {
 
 #define mt_ast_fn_access(fn, target) fn->sym_table.target
 
+static inline mt_ast_bop* mt_ast_create_bop(mt_ast* const left, mt_ast* const right) {
+    mt_ast_bop* bop = (mt_ast_bop*) malloc(sizeof(mt_ast_bop));
+    bop->left = left;
+    bop->right = right;
+    return bop;
+}
+
 static mt_ast* mt_ast_fn_init(void) {
     mt_ast_fn* fn = malloc(sizeof(mt_ast_fn));
     mt_ast_fn_access(fn, arg_table).hash = NULL;
@@ -52,9 +59,9 @@ void mt_ast_init(mt_ast_state* const state) {
     state->ast = mt_ast_fn_init();
 }
 
-static void mt_ast_add_data_to_tree(mt_ast* cur_tree, mt_ast* const new_node) {
+static void mt_ast_add_data_to_tree(mt_ast** const cur_tree, mt_ast* const new_node) {
     if (cur_tree == NULL) {
-        cur_tree = new_node;
+        *cur_tree = new_node;
         return;
     }
 }
@@ -68,7 +75,9 @@ static mt_var mt_ast_token_invalid(const mt_token* const cur_token) {
     return mt_var_err(mt_err_ast_build_fail(err_buf));
 }
 
-static mt_var mt_ast_next_token(mt_ast_state* const state, mt_ast* const cur_tree) {
+#define inc_token(state) state->cur_token = state->cur_token->next
+
+static mt_var mt_ast_next_token(mt_ast_state* const state, mt_ast** const cur_tree) {
     if (state->cur_token == NULL) {
         return mt_var_bool(true);
     }
@@ -90,15 +99,16 @@ static mt_var mt_ast_next_token(mt_ast_state* const state, mt_ast* const cur_tre
         case mt_token(INT):
             mt_ast_add_data_to_tree(cur_tree, mt_ast_node(INT, value, mt_ast_value(mt_int, state->cur_token->data.mt_int)));
             break;
-        case mt_token(NL):
-            if (cur_tree != NULL) {
-                return mt_var_bool(true);
-            }
+        case mt_token(ASSIGN):
+            *cur_tree = mt_ast_node(ASSIGN, bop, mt_ast_create_bop(*cur_tree, NULL));
             break;
+        case mt_token(NL):
+            inc_token(state);
+            return mt_var_bool(true);
         default:
             return mt_ast_token_invalid(state->cur_token);
     }
-    state->cur_token = state->cur_token->next;
+    inc_token(state);
     return mt_ast_next_token(state, cur_tree);
 }
 
@@ -106,7 +116,7 @@ mt_var mt_ast_build(mt_ast_state* const state, mt_token* const tokens) {
     state->cur_token = tokens;
     while (state->cur_token != NULL) {
         mt_ast* cur_tree = NULL;
-        mt_var rst = mt_ast_next_token(state, cur_tree);
+        mt_var rst = mt_ast_next_token(state, &cur_tree);
         if (mt_var_is_err(rst)) {
             return rst;
         }
@@ -114,6 +124,8 @@ mt_var mt_ast_build(mt_ast_state* const state, mt_token* const tokens) {
             state->ast->node.fn->ops_tail->op = cur_tree;
             state->ast->node.fn->ops_tail->next = mt_ast_add_op_list();
             state->ast->node.fn->ops_tail = state->ast->node.fn->ops_tail->next;
+            // @TODO print tree
+            exit(1);
         }
     }
     return mt_var_bool(true);
