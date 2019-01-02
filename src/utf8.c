4faa624ef0420d12ae33221bf1a3f4b24d9c0d24
kcomas <kevin.comas.git@gmail.com>
2019-01-02 00:43:26 -0500

    test utf8

--- src/utf8.c
+++ src/utf8.c
@@ -17,7 +17,10 @@ utf8 utf8_from_c(char c) {
 }
 
 size_t utf8_len(utf8 c) {
-    for (size_t i = 4; i > 0; i--) if (c.b[i] > 0) return i;
+    if ((c.b[0] & 0x80) == 0) return 1;
+    if ((c.b[0] & 0xe0) == 0xc0) return 2;
+    if ((c.b[0] & 0xf0) == 0xe0) return 3;
+    if ((c.b[0] & 0xf8) == 0xf0) return 4;
     return 0;
 }
 
--- test/utf8.c
+++ test/utf8.c
@@ -4,5 +4,11 @@
 
 int main(void) {
     test("Int value of a is 97", utf8_value(utf8_from_c('a')), ==, 97);
+    utf8 sigma = utf8_init(0xce, 0xa3, 0, 0);
+    test("Sigma length is 2", utf8_len(sigma), ==, 2);
+    printf("Sigma: ");
+    utf8_print(sigma);
+    printf("\n");
+    test("Sigma value is 931", utf8_value(sigma), ==, 931);
     return 0;
 }
