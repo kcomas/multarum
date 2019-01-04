4ec62b7769be2a03a8a860b0a3d33c6a86eeb636
kcomas <kevin.comas.git@gmail.com>
2019-01-04 10:52:11 -0500

    add colors to tests

---
+++ test/vec.c
@@ -0,0 +1,7 @@
+
+#include "vec.h"
+#include "test.h"
+
+int main(void) {
+    return 0;
+}
--- tests.sh
+++ tests.sh
@@ -2,23 +2,39 @@
 
 make tests
 
-echo "RUNNING TESTS"
+bold() {
+    echo "\e[1m$1\e[0m"
+}
+
+bold_blue() {
+    echo "\e[1;34m$1\e[0m"
+}
+
+bold_green() {
+    echo "\e[1;32m$1\e[0m"
+}
+
+bold_red() {
+    echo "\e[1;31m$1\e[0m"
+}
+
+bold "RUNNING TESTS"
 
 for TEST in *_test
 do
-    echo "RUNNING $TEST"
+    bold_blue "RUNNING $TEST"
     ./$TEST
     if [ $? -ne 0 ]
     then
-        echo "TEST $TEST FAILED"
+        bold_red "TEST $TEST FAILED"
         exit 1
     fi
     valgrind --leak-check=full --error-exitcode=12 ./$TEST
     if [ $? -eq 12 ]
     then
-        echo "TEST $TEST HAS MEMORY ERRORS"
+        bold_red "TEST $TEST HAS MEMORY ERRORS"
         exit 1
     fi
 done
 
-echo "ALL TESTS PASSED"
+bold_green "ALL TESTS PASSED"
