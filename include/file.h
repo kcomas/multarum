6f94bc2aa7f20052234878aa6d322020562e1463
kcomas <kevin.comas.git@gmail.com>
2019-01-08 16:42:51 -0500

    file ops

--- include/file.h
+++ include/file.h
@@ -2,12 +2,12 @@
 #ifndef FILE_H
 #define FILE_H
 
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <dirent.h>
 #include "var.h"
 
-bool file_to_str(str pathname, var* err, str* f);
+int open_file(str pathname);
+
+bool file_to_str(int fd, var* err, str* s);
 
 #endif
