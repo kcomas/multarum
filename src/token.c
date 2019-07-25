20b5786a1fc4d32a276c4a691b28607a32868279
kcomas <kevin.comas.git@gmail.com>
2019-07-24 21:19:40 -0400

    add tokens in hash

--- src/token.c
+++ src/token.c
@@ -1,6 +1,48 @@
 
 #include "token.h"
 
+static const char *token_name[] = {
+    "NONE",
+    "FN",
+    "NAME",
+    "INTEGER",
+    "TYPE",
+    "LBRACE",
+    "RBRACE",
+    "RBRACKET",
+    "LBRACKET",
+    "COLON",
+    "IF",
+    "ELIF",
+    "ELSE",
+    "RETURN",
+    "PRINT",
+    "LESS_THAN",
+    "EQUAL",
+    "OR",
+    "ADD",
+    "SUB",
+    "END"
+};
+
+static const char *token_word[] = {
+    "fn",
+    "if",
+    "elif",
+    "else",
+    "return",
+    "print"
+};
+
+static const token_type token_word_type[] = {
+    TOKEN(FN),
+    TOKEN(IF),
+    TOKEN(ELIF),
+    TOKEN(ELSE),
+    TOKEN(RETURN),
+    TOKEN(PRINT)
+};
+
 token_state *tokenize_file(const char *filename, char **const err) {
     struct stat s;
     if (stat(filename, &s) == -1) {
@@ -12,4 +54,24 @@ token_state *tokenize_file(const char *filename, char **const err) {
         *err = "Cannot Open File";
         return NULL;
     }
+    token_state *ts = calloc(1, sizeof(token_state) + sizeof(char) * s.st_size);
+    if (read(fd, ts->str, s.st_size) != s.st_size) {
+        free(ts);
+        *err = "Unable To Read File";
+        return NULL;
+    }
+    for (size_t i = 0; i < TOKEN_WORD_LEN; i++) {
+        const char *w = token_word[i];
+        char c;
+        size_t hash = 5381;
+        while ((c = *w++)) hash = ((hash << 5) + hash) + c;
+        hash %= TOKEN_WORD_HASH_LEN;
+        if (ts->token_word_hash[hash] != 0) {
+            free(ts);
+            *err = "Token Hash Collision";
+            return NULL;
+        }
+        ts->token_word_hash[hash] = token_word_type[i];
+    }
+    return ts;
 }
--- src/token.h
+++ src/token.h
@@ -1,13 +1,14 @@
 
 #pragma once
 
-#define TOKEN(NAME) TOKEN_##NAME
-
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+
+#define TOKEN(NAME) TOKEN_##NAME
 
 typedef enum {
     TOKEN(NONE),
@@ -30,7 +31,8 @@ typedef enum {
     TOKEN(OR),
     TOKEN(ADD),
     TOKEN(SUB),
-    TOKEN(END)
+    TOKEN(END),
+    TOKEN(_LEN)
 } token_type;
 
 typedef struct _token {
@@ -39,14 +41,17 @@ typedef struct _token {
     const char *value;
 } token;
 
+#define TOKEN_WORD_LEN 6
+
+#define TOKEN_WORD_HASH_LEN TOKEN_WORD_LEN * 4
+
 typedef struct _token_state {
     token_type last_match_type;
     size_t last_match_start_idx, last_match_end_idx;
     size_t line_idx, char_idx, str_len;
-    const char *str;
+    token_type token_word_hash[TOKEN_WORD_HASH_LEN];
+    char str[];
 } token_state;
-
-token_state *tokenize_str(const char *str, size_t len, char **const err);
 
 token_state *tokenize_file(const char *filename, char **const err);
 
