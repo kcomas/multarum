b15241ed77923b2419491ab8b51574c729c27f47
kcomas <kevin.comas.git@gmail.com>
2019-08-18 20:00:50 -0400

    new parser structure

--- src/parser.h
+++ src/parser.h
@@ -3,3 +3,44 @@
 
 #include <stdint.h>
 #include "token.h"
+
+#define AST(NAME) AST_##NAME
+
+typedef enum {
+
+} ast_type;
+
+typedef struct _ast_node {
+    ast_type type;
+    union {
+
+    } data;
+} ast_node;
+
+#define PARSER_NODE(NAME) PARSER_NODE_##NAME
+
+typedef enum {
+    PARSER_NODE(MATCH),
+    PARSER_NODE(NODE)
+} parser_node_type;
+
+typedef struct _parser_node {
+    parser_node_type type;
+    union {
+        struct {
+            token_type token_type;
+        } match;
+        struct {
+
+        } node;
+    } date;
+} parser_node;
+
+typedef struct _token_list {
+    token t;
+    struct _token_list *next;
+} token_list;
+
+typedef struct {
+
+} parser_state;
